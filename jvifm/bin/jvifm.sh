550f0a21d87b9179534faea9dcddb43ab270ad71
wangsong2 <wangsong2@fca8e7a3-3947-0410-adac-e60ac8084ff7>
2010-05-30 00:34:21 +0000

    git-svn-id: https://jvifm.svn.sourceforge.net/svnroot/jvifm/trunk@89 fca8e7a3-3947-0410-adac-e60ac8084ff7

--- jvifm/bin/jvifm.sh
+++ jvifm/bin/jvifm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd /soft/jvifm/dist
+cd /project/jvifm/dist
 
 java -cp ../lib/jintellitype-1.3.1.jar:../lib/jxgrabkey.jar:../lib/commons-vfs-20070730.jar:../lib/lucene-core-2.2.0.jar:../lib/commons-io-1.4.jar:../lib/commons-cli-1.1.jar:../lib/commons-logging.jar:../lib/dom4j.jar:../lib/jaxen-1.1-beta-9.jar:../lib/log4j-1.2.8.jar:../lib/swt-linux/swt.jar:../lib/jvifm.jar:../lib/apache-tool.jar  -Djava.library.path=../lib net.sf.jvifm.Main
